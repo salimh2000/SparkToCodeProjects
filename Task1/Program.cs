e3757a3f36fd2c652ef2c12e3778f447ae55c1b7
s0586805 <s0586805@htw-berlin.de>
2026-07-01 16:06:14 +0400

    Add Part1 - Task9

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -145,7 +145,7 @@ class Program
         /////////////////////////////////////////////////////////////////////////////////
         
         // Task 8 –  Restaurant Bill with Membership Discount
-        Console.Write("Enter total bill amount (OMR): ");
+        /*Console.Write("Enter total bill amount (OMR): ");
         double bill = double.Parse(Console.ReadLine());
         
         Console.Write("Are you a loyalty member? (yes/no): ");
@@ -162,8 +162,40 @@ class Program
         
         Console.WriteLine("Original Bill: "  + bill + " OMR");
         Console.WriteLine("Discount: " + discount + " OMR");
-        Console.WriteLine("Amount to Pay: "  + finalBill + " OMR");
+        Console.WriteLine("Amount to Pay: "  + finalBill + " OMR");*/
         
+        /////////////////////////////////////////////////////////////////////////////////
+        
+        // Task 9 –  Day Name Finder
+        Console.Write("Enter a day number (1-7): ");
+        int dayNumber = Convert.ToInt32(Console.ReadLine());
+        switch (dayNumber)
+        {
+            case 1:
+                Console.WriteLine("Sunday");
+                break;
+            case 2:
+                Console.WriteLine("Monday");
+                break;
+            case 3:
+                Console.WriteLine("Tuesday");
+                break;
+            case 4:
+                Console.WriteLine("Wednesday");
+                break;
+            case 5:
+                Console.WriteLine("Thursday");
+                break;
+            case 6:
+                Console.WriteLine("Friday");
+                break;
+            case 7:
+                Console.WriteLine("Saturday");
+                break;
+            default:
+                Console.WriteLine("Invalid day number");
+                break;
+        }
         
         
         
