1b5da5ce70d5b409de0e7b4dfe4a403481490eb4
s0586805 <s0586805@htw-berlin.de>
2026-07-01 15:16:12 +0400

    Add Part1 - Task2

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -5,7 +5,7 @@ class Program
     static void Main(string[] args)
     {
         // Task 1 – Personal Info Card
-        string name = "Sara";
+        /*string name = "Sara";
         int    age = 21;
         double height = 1.65;
         bool   isStudent = true;
@@ -13,6 +13,19 @@ class Program
         Console.WriteLine("Name: " + name);
         Console.WriteLine("Age: " + age);
         Console.WriteLine("Height: " + height);
-        Console.WriteLine("Student: " + isStudent);
+        Console.WriteLine("Student: " + isStudent);*/
+        
+        //  Task 2 – Rectangle Calculator
+        Console.Write("Enter length: ");
+        double length = double.Parse(Console.ReadLine());
+        Console.Write("Enter width: ");
+        double width = double.Parse(Console.ReadLine());
+            
+        double area = length * width;
+        double perimeter = 2 * (length + width);
+            
+        Console.WriteLine("Area: " + area);
+        Console.WriteLine("Perimeter: " + perimeter);
+        
     }
 }
