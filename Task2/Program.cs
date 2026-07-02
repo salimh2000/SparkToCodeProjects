363ea1714ff05ff24fd5f3551e1e790859a05faa
s0586805 <s0586805@htw-berlin.de>
2026-07-02 12:22:03 +0400

    implemented Part2- Task 2 - Sum of Numbers 1 to N

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -5,7 +5,7 @@ class Program
     static void Main(string[] args)
     {
         //Task 1 - Countdown Timer
-        Console.Write("Enter a starting number: ");
+        /*Console.Write("Enter a starting number: ");
         int start = Convert.ToInt32(Console.ReadLine());
             
         for (int i = start; i >= 1; i--)
@@ -13,8 +13,29 @@ class Program
             Console.WriteLine(i);
         }
             
-        Console.WriteLine("Liftoff!");
+        Console.WriteLine("Liftoff!");*/
             
         /////////////////////////////////////////////////////////////////////////////////
+        
+        //Task 2 - Sum of Numbers 1 to N
+        Console.Write("Enter a positive whole number: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+        int sum = 0;
+ 
+        for (int i = 1; i <= n; i++)
+        {
+            sum += i;   
+        }
+        
+        Console.WriteLine("Sum of 1 to " + n + " is " + sum);
+        
+        /////////////////////////////////////////////////////////////////////////////////
+        
+        //Task 3 - Multiplication Table
+        
+        
+        
+        
+
     }
 }
