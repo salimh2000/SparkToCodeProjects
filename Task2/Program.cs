706d257de28fbcf05fe4236d945381ffa2def071
s0586805 <s0586805@htw-berlin.de>
2026-07-02 12:16:41 +0400

    implemented Part2 - Task 1 - Countdown Timer

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        //Task 1 - Countdown Timer
+        Console.Write("Enter a starting number: ");
+        int start = Convert.ToInt32(Console.ReadLine());
+            
+        for (int i = start; i >= 1; i--)
+        {
+            Console.WriteLine(i);
+        }
+            
+        Console.WriteLine("Liftoff!");
+            
+        /////////////////////////////////////////////////////////////////////////////////
     }
 }
